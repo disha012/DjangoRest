d41526460ed732638ea66c85f9577645d0c9d292
disha012 <dishagf@gmail.com>
2017-10-28 07:48:00 +0530

    second commit

---
+++ blog-api/src/posts/templatetags/urlify.py
@@ -0,0 +1,18 @@
+try:
+    from urllib import quote_plus 
+except:
+    pass
+
+try:
+    from urllib.parse import quote_plus 
+except: 
+    pass
+
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def urlify(value):
+    return quote_plus(value)
